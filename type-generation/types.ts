18d5627fffa0d385680fe20aed7f65461fb8ff22
Hood Chatham <roberthoodchatham@gmail.com>
2023-12-02 16:45:12 -0800

    Add forgotten types.ts file

---
+++ type-generation/types.ts
@@ -0,0 +1,87 @@
+import {
+  ClassDeclaration,
+  ConstructSignatureDeclaration,
+  Identifier,
+  InterfaceDeclaration,
+  MethodSignature,
+  ModuleDeclaration,
+  PropertySignature,
+  SyntaxKind,
+  TypeAliasDeclaration,
+  VariableDeclaration,
+  VariableStatement,
+} from "ts-morph";
+
+export type SyntaxKindMap = {
+  [SyntaxKind.VariableStatement]: VariableStatement;
+  [SyntaxKind.VariableDeclaration]: VariableDeclaration;
+  [SyntaxKind.PropertySignature]: PropertySignature;
+  [SyntaxKind.MethodSignature]: MethodSignature;
+  [SyntaxKind.ConstructSignature]: ConstructSignatureDeclaration;
+  [SyntaxKind.InterfaceDeclaration]: InterfaceDeclaration;
+  [SyntaxKind.TypeAliasDeclaration]: TypeAliasDeclaration;
+  [SyntaxKind.ModuleDeclaration]: ModuleDeclaration;
+};
+
+export type GroupedBySyntaxKind = {
+  [K in keyof SyntaxKindMap]?: SyntaxKindMap[K][];
+};
+
+export type InterfacesIdentifier = {
+  kind: "interfaces";
+  name: string;
+  ifaces: InterfaceDeclaration[];
+};
+export type TypeAliasIdentifier = {
+  kind: "typeAlias";
+  name: string;
+  decl: TypeAliasDeclaration;
+};
+export type ClassIdentifier = {
+  kind: "class";
+  name: string;
+  decl: ClassDeclaration;
+  ifaces: InterfaceDeclaration[];
+};
+export type VarDeclIdentifier = {
+  kind: "varDecl";
+  name: string;
+  decl: VariableDeclaration;
+  ifaces: InterfaceDeclaration[];
+};
+
+export type ClassifiedIdentifier =
+  | InterfacesIdentifier
+  | TypeAliasIdentifier
+  | ClassIdentifier
+  | VarDeclIdentifier;
+
+export type Needed =
+  | { type: "ident"; ident: Identifier }
+  | { type: "interface"; ident: Identifier };
+
+export interface PyClass {
+  kind: "class";
+  name: string;
+  superStems: string[];
+  supers: string[];
+  pysupers: string[];
+  body: string;
+  concrete?: boolean;
+}
+
+export interface PyOther {
+  kind: "other";
+  text: string;
+}
+
+export type PyTopLevel = PyClass | PyOther;
+
+export enum Variance {
+  covar = 1,
+  contra = -1,
+  none = 0,
+}
+export function reverseVariance(v: Variance): Variance {
+  return -v;
+}
