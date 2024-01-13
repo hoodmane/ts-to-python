6faf7f01c68f2c74993f39ce42af08c7b3ad7c56
Hood Chatham <roberthoodchatham@gmail.com>
2024-01-13 07:59:02 -0800

    Add type IR and conversion from type ast to type IR

---
+++ type-generation/astUtils.ts
@@ -0,0 +1,138 @@
+import { ConstructSignatureDeclaration, EntityName, FunctionTypeNode, Identifier, InterfaceDeclaration, MethodSignature, Node, PropertySignature, Signature, SyntaxKind, TypeAliasDeclaration, TypeArgumentedNode, TypeElementTypes, TypeNode, TypeParameterDeclaration } from "ts-morph";
+import { WrappedGen, groupBy, groupByGen, split } from "./groupBy";
+import { ClassifiedIdentifier, GroupedBySyntaxKind } from "./types";
+
+export function getNodeLocation(node: Node): string {
+  const sf = node.getSourceFile();
+  const { line, column } = sf.getLineAndColumnAtPos(node.getStart());
+  return `${sf.getFilePath()}:${line}:${column}`;
+}
+
+export function groupBySyntaxKind(list: Iterable<Node>): GroupedBySyntaxKind {
+  const gen = groupBySyntaxKindGen();
+  for (const x of list) {
+    gen.next(x);
+  }
+  return gen.done();
+}
+
+export function groupBySyntaxKindGen(): WrappedGen<Node, GroupedBySyntaxKind> {
+  return groupByGen<Node, any>((node) => {
+    return node.getKind();
+  });
+}
+
+export function groupMembers(members: TypeElementTypes[]): {
+  methods: Record<string, Signature[]>;
+  properties: PropertySignature[];
+  constructors: ConstructSignatureDeclaration[];
+} {
+  const grouped = groupBySyntaxKind(members);
+  const allProperties = grouped[SyntaxKind.PropertySignature] || [];
+  const { functions = [], properties = [] } = groupBy(allProperties, (prop) =>
+    prop.getTypeNode()?.isKind(SyntaxKind.FunctionType)
+      ? "functions"
+      : "properties",
+  );
+  const methodSigs = grouped[SyntaxKind.MethodSignature] || [];
+  const empty: [string, FunctionTypeNode | MethodSignature][] = [];
+  const methodOrFuncProps = empty.concat(
+    methodSigs.map((meth) => [meth.getName(), meth]),
+    functions.map((func) => [
+      func.getName(),
+      func.getTypeNode() as FunctionTypeNode,
+    ]),
+  );
+  const methodNamePairs = groupBy(methodOrFuncProps, ([name, prop]) => name);
+  const methods = Object.fromEntries(
+    Object.entries(methodNamePairs).map(([name, v]) => [
+      name,
+      v.map(([_, prop]) => prop.getSignature()),
+    ]),
+  );
+  const constructors = grouped[SyntaxKind.ConstructSignature] || [];
+  return { methods, properties, constructors };
+}
+
+export function getExpressionTypeArgs(
+  ident: EntityName,
+  expression: TypeArgumentedNode & Node,
+): TypeNode[] {
+  if (Node.isQualifiedName(ident)) {
+    ident = ident.getRight();
+  }
+  const typeArgNodes = expression.getTypeArguments();
+  const numTypeArgs = expression.getType().getTypeArguments().length;
+  if (typeArgNodes.length < numTypeArgs) {
+    const seenNames: string[] = [];
+    const paramDecls: TypeParameterDeclaration[] = [];
+    const defs = ident
+      .getDefinitionNodes()
+      .filter(
+        (node): node is InterfaceDeclaration | TypeAliasDeclaration =>
+          Node.isInterfaceDeclaration(node) ||
+          Node.isTypeAliasDeclaration(node),
+      );
+    for (const def of defs) {
+      const params = def.getTypeParameters();
+      for (const param of params) {
+        const paramName = param.getName();
+        if (!seenNames.includes(paramName)) {
+          seenNames.push(paramName);
+          paramDecls.push(param);
+        }
+      }
+    }
+    const missingDecls = paramDecls.slice(-(numTypeArgs - typeArgNodes.length));
+    for (const decl of missingDecls) {
+      typeArgNodes.push(decl.getDefaultOrThrow());
+    }
+  }
+  return typeArgNodes;
+}
+
+export function classifyIdentifier(ident: Identifier): ClassifiedIdentifier {
+  let name = ident.getText();
+  const defs = ident.getDefinitionNodes();
+  const [ifaces, rest] = split(defs, Node.isInterfaceDeclaration);
+  if (rest.length === 0) {
+    name += "_iface";
+    return {
+      kind: "interfaces",
+      name,
+      ifaces,
+    };
+  }
+  if (rest.length > 1) {
+    throw new Error("Oops!");
+  }
+  const decl = rest[0];
+  if (Node.isClassDeclaration(decl)) {
+    return {
+      kind: "class",
+      name,
+      decl,
+      ifaces,
+    };
+  }
+  if (Node.isVariableDeclaration(decl)) {
+    return {
+      kind: "varDecl",
+      name,
+      decl,
+      ifaces,
+    };
+  }
+
+  if (Node.isTypeAliasDeclaration(decl)) {
+    if (ifaces.length > 0) {
+      throw new Error("Both interfaces and type aliases with same name...");
+    }
+    return {
+      kind: "typeAlias",
+      name,
+      decl,
+    };
+  }
+  throw new Error("Unrecognized ident!");
+}
--- type-generation/tests/a.test.ts
+++ type-generation/tests/a.test.ts
@@ -2,39 +2,7 @@ import { SyntaxKind } from "ts-morph";
 import { Converter } from "../extract.ts";
 import { renderPyClass } from "../render.ts";
 import { PyClass, PyOther, Variance } from "../types.ts";
-
-let n = 0;
-function getTypeNode(converter: Converter, type) {
-  n++;
-  const fname = `/getTypeNode_$${n}.ts`;
-  converter.project.createSourceFile(fname, `type A = ${type};`);
-  const file = converter.project.getSourceFileOrThrow(fname);
-  const alias = file.getFirstDescendantByKind(SyntaxKind.TypeAliasDeclaration);
-  return alias.getTypeNode();
-}
-
-function removeTypeIgnores(a: string) {
-  return a.replaceAll(/\s*#.*$/gm, "");
-}
-
-function dedent(s) {
-  const lines = s.split("\n");
-  let numSpaces = Infinity;
-  for (const line of lines) {
-    if (/^\s*$/.test(line)) {
-      continue;
-    }
-    numSpaces = Math.min(numSpaces, /^\s*/.exec(line)[0].length);
-  }
-  return lines
-    .map((line) => {
-      if (/^\s*$/.test(line)) {
-        return "";
-      }
-      return line.slice(numSpaces);
-    })
-    .join("\n");
-}
+import { dedent, getTypeNode, removeTypeIgnores } from "./helpers.ts";
 
 function checkTypeToPython(
   converter: Converter,
