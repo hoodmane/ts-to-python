421d3611ff93d3a49992e780e4edea7a098f91fc
Hood Chatham <roberthoodchatham@gmail.com>
2025-10-09 11:47:58 -0400

    Update readme (#105)

---
+++ generate_types.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p generated/js/
+npm run -C type-generation build ../input-projects/browser-dom/ ../generated/js/__init__.pyi
+(cd input-projects/cloudflare-workers/ && npx wrangler types)
+npm run -C type-generation build ../input-projects/cloudflare-workers/ ../generated/js/cloudflare.pyi
